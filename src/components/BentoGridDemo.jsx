664f87d900fbf0d4a5c9313827bc7808fa85398d
Arnab Sarkar <110808978+arnab236@users.noreply.github.com>
2024-05-18 01:09:23 +0530

    Update BentoGridDemo.jsx

--- src/components/BentoGridDemo.jsx
+++ src/components/BentoGridDemo.jsx
@@ -33,37 +33,37 @@ const items = [
   {
     title: "Interhackative Prizes",
     description: "Prizes of inter college hackathon distribution.",
-    header: <img src="./news1.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src="../assets/news1.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "Arcadia ’23",
     description: " AOT unleashed its inner rhythm to witness an enthralling journey.",
-    header: <img src="./news20.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src="../assets/news20.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "Synapse",
     description: "A small quiz competition hosted by SC ECE.",
-    header: <img src="./news2.jpg " className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src="../assets/news2.jpg " className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "Techfiesta ’24",
     description:"All the tech,cultural and exciting events in one zone.",
-    header: <img src="./news21.jpg " className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src="../assets/news21.jpg " className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "Arcadia ’23",
     description: "One of the biggest cultural fest in West Bengal.",
-    header: <img src="./news13.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src="../assets/news13.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "The Joy of Creation",
     description: "Experience the thrill of bringing ideas to life.",
-    header: <img src="./news4.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src="../assets/news4.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
 
   },
 
