0e2972468ecd7e8dc8b18e95ba103265f1d11f4e
Arnab Sarkar <110808978+arnab236@users.noreply.github.com>
2024-05-18 18:09:04 +0530

    Update BentoGridDemo.jsx

--- src/components/BentoGridDemo.jsx
+++ src/components/BentoGridDemo.jsx
@@ -12,6 +12,13 @@ import {
 } from "@tabler/icons-react";
 import { Link,NavLink } from "react-router-dom";
 
+import news1 from "../assets/news1.jpg";
+import news2 from "../assets/news20.jpg";
+import news3 from "../assets/news2.jpg";
+import news4 from "../assets/news21.jpg";
+import news7 from "../assets/news13.jpg";
+import news8 from "../assets/news4.jpg";
+
 export function BentoGridDemo() {
   return (
     <BentoGrid className="max-w-3xl md:max-w-7xl mx-auto p-4" >
@@ -33,37 +40,37 @@ const items = [
   {
     title: "Interhackative Prizes",
     description: "Prizes of inter college hackathon distribution.",
-    header: <img src="../assets/news1.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={news1} className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "Arcadia ’23",
     description: " AOT unleashed its inner rhythm to witness an enthralling journey.",
-    header: <img src="../assets/news20.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={news20} className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "Synapse",
     description: "A small quiz competition hosted by SC ECE.",
-    header: <img src="../assets/news2.jpg " className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={news2} className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "Techfiesta ’24",
     description:"All the tech,cultural and exciting events in one zone.",
-    header: <img src="../assets/news21.jpg " className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={news21} className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "Arcadia ’23",
     description: "One of the biggest cultural fest in West Bengal.",
-    header: <img src="../assets/news13.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={news13} className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "The Joy of Creation",
     description: "Experience the thrill of bringing ideas to life.",
-    header: <img src="../assets/news4.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={news4} className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
 
   },
 
