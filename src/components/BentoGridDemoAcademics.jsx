7fdf5ccb2c7dc2a1fa6b37ed0fdb247f45a08791
Arnab Sarkar <110808978+arnab236@users.noreply.github.com>
2024-05-18 18:04:14 +0530

    Update BentoGridDemoAcademics.jsx

--- src/components/BentoGridDemoAcademics.jsx
+++ src/components/BentoGridDemoAcademics.jsx
@@ -11,6 +11,11 @@ import {
   IconTableColumn,
 } from "@tabler/icons-react";
 
+import deptnews1 from "../assets/deptNews1.png";
+import deptnews2 from "../assets/deptNews2.jpeg";
+import deptnews3 from "../assets/deptNews3.jpeg";
+import deptnews4 from "../assets/deptNews4.jpg";
+
 export function BentoGridDemoAcademics() {
   return (
     <BentoGrid className="max-w-3xl md:max-w-7xl mx-auto p-4" >
@@ -31,25 +36,25 @@ const items = [
   {
     title: "Students Chapter ECE",
     description: "From Vision to Victory: Navigating the Highlights",
-    header: <img src="../assets/deptNews1.png" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={deptnews1} className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "Students Chapter CSE",
     description: "Legacy Unleashed: A Pinnacle of Excellence.",
-    header: <img src="../assets/deptNews2.jpeg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={deptnews2} className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "IEI Students Chapter ME",
     description: "Seminar on 3D printing and prosthetics",
-    header: <img src="../assets/deptNews3.jpeg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={deptnews3} className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "IEEE",
     description: "Inauguration of IEEE Student Branch at AOT",
-    header: <img src="../assets/deptNews4.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={deptnews4} className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
 
