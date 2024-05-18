2cd7f2448c38a804c0606c5ed15ab7fe0634553a
Arnab Sarkar <110808978+arnab236@users.noreply.github.com>
2024-05-18 18:13:45 +0530

    Update BentoGridDemoOne.jsx

--- src/components/BentoGridDemoOne.jsx
+++ src/components/BentoGridDemoOne.jsx
@@ -11,6 +11,13 @@ import {
   IconTableColumn,
 } from "@tabler/icons-react";
 
+import news7 from "../assets/news3.jpg";
+import news8 from "../assets/news22.jpg";
+import news9 from "../assets/news28.jpeg";
+import news10 from "../assets/news24.jpg";
+import news11 from "../assets/news17.jpg";
+import news12 from "../assets/news29.jpg";
+
 export function BentoGridDemoOne() {
   return (
     <BentoGrid className="max-w-3xl md:max-w-7xl mx-auto p-4" >
@@ -32,38 +39,38 @@ const items = [
   {
     title: "The Dawn of Innovation",
     description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <img src="../assets/news3.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={news7} className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "StartUp Talks by two of our shining students.",
     description: "A valuable experience of their journey to build a startup given by Ronit and Subhrokamal.",
-    header: <img src="../assets/news22.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={news8} className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "SIH 2023",
     description: "A remarkable contribution by students.",
-    header: <img src="../assets/news28.jpeg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={news9} className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "AOT-Agile",
     description:
       "A great sports torunament organised in AOT.",
-    header: <img src="../assets/news24.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={news10} className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "The Pursuit of Knowledge",
     description: "Join the quest for understanding and enlightenment.",
-    header: <img src="../assets/news17.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={news11}className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
     
   },
   {
     title: "Techtrix",
     description: "Experience the thrill of bringing ideas to life.",
-    header: <img src="../assets/news29.jpg" className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
+    header: <img src={news12} className=" w-full bg-auto bg-center object-cover overflow-hidden"/>,
 
   },
 
