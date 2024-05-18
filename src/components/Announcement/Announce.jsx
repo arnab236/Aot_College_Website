d11bfd2852fb761dd9b94c6217ebde6d8e7612b0
Arnab Sarkar <110808978+arnab236@users.noreply.github.com>
2024-05-18 17:35:37 +0530

    Update Announce.jsx

--- src/components/Announcement/Announce.jsx
+++ src/components/Announcement/Announce.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Notice from './Notice'
-
+import announce from "../../assets/ann_logo.png";
 
 
 function Announce() {
@@ -10,7 +10,7 @@ function Announce() {
   return (
     <div className='bottom-0 right-0'>
     <button onClick={() => {setNoticed(true)}} className='rounded-full relative h-16 w-16 text-xl bg-[#28186b] [box-shadow:12px_12px_20px_#aaaaaa,_-8px_-8px_20px_#aaaaaa] text-white overflow-hidden flex items-center justify-center' >        
-      <img src="src/assets/ann_logo.png" className='filter absolute invert left-2' alt="" />
+      <img src={announce} className='filter absolute invert left-2' alt="" />
     </button>
     {Noticed && <Notice onClose= {() => {setNoticed(false)}} />}
     </div>
