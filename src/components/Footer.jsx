bd723d1207489074b279631ff06dabe9cf379c24
Arnab Sarkar <110808978+arnab236@users.noreply.github.com>
2024-07-20 05:53:54 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import "../index.css"
 
 const Footer = () => {
   return (
-    <footer className="bg-[linear-gradient(to_right_bottom,rgba(45,38,83,0.3),rgba(65,54,255,0.6)),url('src/assets/footergbg.jpg')]  bg-center bg-cover font-['para'] " >
+    <footer className="bg-[linear-gradient(to_right_bottom,rgba(45,38,83,0.3),rgba(65,54,255,0.6)),url('../assets/footergbg.jpg')]  bg-center bg-cover font-['para'] " >
     <div className="bg-[rgba(0,0,0,0.2)] py-12">
       <div className="container mx-auto flex justify-center">
         <div className="footer-content w-full md:w-1/3 text-center">
