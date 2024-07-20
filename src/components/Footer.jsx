9009408e21bb79d4f33b90ee328908ccb77a547a
Arnab Sarkar <110808978+arnab236@users.noreply.github.com>
2024-07-20 14:30:25 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,14 @@ import FooterBg from "../assets/footergbg.jpg"
 
 const Footer = () => {
   return (
-    <footer className=' h-[100px] font-[para]'>
-      <img src={FooterBg} className="w-full h-[36%] z-0 absolute bg-[linear-gradient(to_right_bottom,rgba(45,38,83,0.3),rgba(65,54,255,0.6))]  z-1 bg-cover" />
+    // <footer className=' h-[100px] font-[para]'>
+{/*       <img src={FooterBg} className="w-full h-[36%] z-0 absolute bg-[linear-gradient(to_right_bottom,rgba(45,38,83,0.3),rgba(65,54,255,0.6))]  z-1 bg-cover" /> */}
+    <footer className="relative h-[100px]">   
+    <img 
+        src={FooterBg} 
+        className="w-full h-full object-cover absolute top-0 left-0 z-0" 
+        alt="Footer Background" 
+      />
       <div className='w-full h-[36%] absolute z-5 bg-[linear-gradient(to_right_bottom,rgba(45,38,83,0.3),rgba(65,54,255,0.6))] '></div>
     <div className="bg-[rgba(0,0,0,0.2)] py-12 z-10 relative">
       <div className="container mx-auto flex justify-center ">
