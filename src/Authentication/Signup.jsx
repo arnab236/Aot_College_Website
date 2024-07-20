db0f9e6651516d09d7beb6ea8cdabb2f504c2a88
Arnab Sarkar <110808978+arnab236@users.noreply.github.com>
2024-07-20 15:29:58 +0530

    Update Signup.jsx

--- src/Authentication/Signup.jsx
+++ src/Authentication/Signup.jsx
@@ -37,7 +37,7 @@ function Signup() {
             sign In
           </div>
           <form
-            onSubmit={handleSubmit}
+{/*             onSubmit={handleSubmit} */}
             className=" flex flex-col justify-between items-center"
           >
             <div class="input-container">
